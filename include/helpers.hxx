b7ec8185af54cdaf53825ca50e2a2706444cf365
Nastassia P. <violificate@gmail.com>
2026-01-19 10:14:57 +0100

    Update helpers.hxx

--- include/helpers.hxx
+++ include/helpers.hxx
@@ -4,5 +4,16 @@
 
 #ifndef NETSIM_HELPERS_HXX
 #define NETSIM_HELPERS_HXX
+#include <functional>
+#include <random>
+
+#include "types.hxx"
+
+extern std::random_device rd;
+extern std::mt19937 rng;
+
+extern double default_probability_generator();
+
+extern ProbabilityGenerator probability_generator;
 
 #endif //NETSIM_HELPERS_HXX
