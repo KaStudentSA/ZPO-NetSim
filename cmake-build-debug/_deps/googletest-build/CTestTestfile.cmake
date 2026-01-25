f508046bc235a993cbc6b9378411687010b85172
Kas <kacpersali@gmail.com>
2026-01-25 01:28:09 +0100

    'nodes'

---
+++ cmake-build-debug/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/studia/ZPO_netsim/ZPO-NetSim/cmake-build-debug/_deps/googletest-src
+# Build directory: C:/studia/ZPO_netsim/ZPO-NetSim/cmake-build-debug/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
